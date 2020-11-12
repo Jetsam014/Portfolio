5d2a8989b79bbfb38c20e0ca657bea5eb0b6d3c4
Flotsam <neroexplosionalbatrossblindfold@yahoo.co.jp>
2020-11-13 02:00:34 +0900

    Created overall

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def message
+  end
+
+  def menu
+  end
+
+  def access
+  end
+
+  def recruit
+  end
+
+  def info
+  end
+end
